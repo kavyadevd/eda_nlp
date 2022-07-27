727a566f9ec519d11d6ce1a9671379766a1c5672
Kavyashree <kavya27.devadiga@gmail.com>
2022-07-27 09:32:45 -0500

    To resolve invalid index error
    when parts is empty skip

--- code/augment.py
+++ code/augment.py
@@ -59,11 +59,12 @@ def gen_eda(train_orig, output_file, alpha_sr, alpha_ri, alpha_rs, alpha_rd, num
 
     for i, line in enumerate(lines):
         parts = line[:-1].split('\t')
-        label = parts[0]
-        sentence = parts[1]
-        aug_sentences = eda(sentence, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
-        for aug_sentence in aug_sentences:
-            writer.write(label + "\t" + aug_sentence + '\n')
+        if(len(parts)>1):
+            label = parts[0]
+            sentence = parts[1]
+            aug_sentences = eda(sentence, alpha_sr=alpha_sr, alpha_ri=alpha_ri, alpha_rs=alpha_rs, p_rd=alpha_rd, num_aug=num_aug)
+            for aug_sentence in aug_sentences:
+                writer.write(label + "\t" + aug_sentence + '\n')
 
     writer.close()
     print("generated augmented sentences with eda for " + train_orig + " to " + output_file + " with num_aug=" + str(num_aug))
